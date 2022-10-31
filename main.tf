6bee2093971e821836aad31111adde2b4ab185fa
Prakash Rao <mb16@iitbbs.ac.in>
2022-11-01 00:29:39 +0900

    add app server instance resource and install file

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "aws_key_pair" "aws-key" {
     public_key = "${file(var.PUBLIC_KEY_PATH)}"
   
 }
+
+resource "aws_instance" "mattermost-app-server" {
+    ami = "${var.AMI_ID}"
+    instance_type = "${var.INSTANCE_TYPE}"
+    subnet_id = "${aws_subnet.mattermost-public-subnet-1.id}"
+    vpc_security_group_ids = ["${aws_security_group.sg_mattermost-app-server.id}"]
+}
